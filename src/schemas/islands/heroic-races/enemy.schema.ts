70a79b9168c54e9d85499393403b8ebef31720bf
Marcuth <thiago.playgames15@gmail.com>
2026-02-11 20:05:27 -0300

    fix heroicRacesEnemySchema

--- src/schemas/islands/heroic-races/enemy.schema.ts
+++ src/schemas/islands/heroic-races/enemy.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 
 export const heroicRacesEnemySchema = z.object({
     id: z.number(),
-    dragon_ids: z.array(z.string()),
+    dragon_ids: z.array(z.coerce.number()),
     handicaps: z.array(z.number()),
     view_type: z.string(),
     elements_view_type: z.array(z.any()),
