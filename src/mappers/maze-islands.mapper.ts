f2d7e7c6a5c9d58d1ea93c8cbae24dfa01fe03f8
Marcuth <thiago.playgames15@gmail.com>
2026-02-18 13:54:58 -0300

    remove logs

--- src/mappers/maze-islands.mapper.ts
+++ src/mappers/maze-islands.mapper.ts
@@ -23,10 +23,7 @@ export class MazeIslandsMapper {
             encounters: data.encounters,
             enemies: data.enemies,
             islands: data.islands.map(island => this.localization.translate(mazeIslandSchema.parse(island))),
-            nodes: data.nodes.map((node, i) => {
-                console.log("Node", i, node)
-                return this.localization.translate(mazeIslandsNodeSchema.parse(node))
-            }),
+            nodes: data.nodes.map(node => this.localization.translate(mazeIslandsNodeSchema.parse(node))),
             parameters: data.parameters,
         } satisfies Record<keyof MazeIslandsDto, any>
     }
